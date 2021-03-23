d5741542ad567b47c1f0d9506a70243df2433fd2
Trend20 <enockomondi305@gmail.com>
2021-03-23 17:18:02 +0300

    fixing the donate button

--- js/main.js
+++ js/main.js
@@ -17,3 +17,16 @@ function topFunction() {
   document.body.scrollTop = 0; 
   document.documentElement.scrollTop = 0; 
 } 
+
+// donate button functionality
+// document.querySelector('.btn').addEventListener('click', makeDonation);
+
+// function makeDonation(e){
+
+// }
+// <form action="https://www.paypal.com/donate" method="post" target="_top">
+// <input type="hidden" name="hosted_button_id" value="G7H9RTJP4KJM2" />
+// <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" />
+// <img alt="" border="0" src="https://www.paypal.com/en_KE/i/scr/pixel.gif" width="1" height="1" />
+// </form>
+
